0c8e492595acfe8d3113ddf90cb3255a831b3c5d
Dmytro Krasnoholovenko <errisi.v@gmail.com>
2024-03-25 01:13:14 +0200

    fixed api move logic, added recursive folder movement with children

--- app/api/files/move/[id]/route.ts
+++ app/api/files/move/[id]/route.ts
@@ -1,6 +1,7 @@
 import { connectToDb } from "@/app/db_connection";
 import { Files } from "@/app/models/file.model";
 import { NextResponse } from "next/server";
+import { Op } from 'sequelize';
 
 export async function PATCH(req: Request, { params }: any) {
   const sequelize = await connectToDb();
@@ -8,19 +9,47 @@ export async function PATCH(req: Request, { params }: any) {
   const id = idParams;
   const { path } = await req.json();
 
-  const file = await sequelize.models.Files.findByPk(id) as Files;
+  const initialFile = await sequelize.models.Files.findByPk(id) as Files;
 
-  if (!file) {
+  if (!initialFile) {
     return NextResponse.json(
       { error: "Not Found" },
       { status: 404 }
     );
   }
 
-  file.path = path;
-  file.save();
+  const moveFile = async (file: Files, newParentPath: string) => {
+    const searchPath = file.path !== '/' ? `${file.path}/${file.title}` : '/' + file.title;
+    const filesFromPath =
+      await sequelize.models.Files.findAll({ where: { path: searchPath, ownerId: file.ownerId } }) as Files[];
 
-  console.log(file);
+    for (const currentFile of filesFromPath) {
+      const relativePath = currentFile.path.substring(file.path.length);
 
-  return NextResponse.json(file);
+      const updatedPath = newParentPath !== '/' ?
+        `${newParentPath}${relativePath.startsWith('/') ? '' : '/'}${relativePath}` :
+        `${relativePath.startsWith('/') ? '' : '/'}${relativePath}`;
+
+      console.log(`relatile = ${relativePath}, updatedPath = ${updatedPath}`)
+
+      if (currentFile.type === 'folder') {
+        await moveFile(currentFile, updatedPath);
+      } else {
+        currentFile.path = updatedPath;
+        await currentFile.save();
+      }
+    }
+
+    file.path = newParentPath;
+    await file.save();
+  }
+
+  if (initialFile.type === 'folder') {
+    await moveFile(initialFile, path);
+  } else {
+    initialFile.path = path;
+    await initialFile.save();
+  }
+
+  return NextResponse.json(initialFile);
 }
